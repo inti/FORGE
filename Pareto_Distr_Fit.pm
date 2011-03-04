82a3a19623b34a3ba23864d9cbb9a6dc8ca0c247
inti <intipedroso@gmail.com>
2011-03-04 17:49:26 +0000

    Major changes - removed routines that are not longer used to fit the GPD - minor changes and bugs fixed
    Signed-off-by: inti <intipedroso@gmail.com>

--- Pareto_Distr_Fit.pm
+++ Pareto_Distr_Fit.pm
@@ -1,5 +1,6 @@
 package Pareto_Distr_Fit;
 use strict;
+use diagnostics;
 use PDL;
 use PDL::Matrix;
 use PDL::NiceSlice;
@@ -8,17 +9,12 @@ use PDL::Stats::Basic;
 use PDL::Stats::Distr;
 use PDL::LinearAlgebra qw(mchol);
 
-#use PDL::Opt::Simplex;
-use PDL::Complex;
-
 our (@EXPORT, @EXPORT_OK, %EXPORT_TAGS);
 
 @EXPORT = qw( Pgpd );				# symbols to export by default
 @EXPORT_OK = qw( Pgpd );			# symbols to export on request
 
 
-our ($z);
-$z = null;
 sub Pgpd {
 =h
 	 Computing permutation test P-value of the GPD approximation
@@ -39,25 +35,40 @@ sub Pgpd {
 	my $alpha = shift;
 	my $N = $y->nelem;
 	my $Padth = 0.05;
-
+	
 	if ($Nexc >= $N-1) {
 		$Nexc = int(0.1*$N);
 	}
+	
+	#print "$N $Nexc $alpha\n";
+	
 	my ($PW2_less_005,$PA2_less_005,$W2,$A2,$cov,$a,$k,$t,$frac) = undef;
+	my $fitted = 0;
 	do {
 		$y = pdl reverse list qsort $y;
 		# Defining the tail	
-		$z = $y->(1:$Nexc);
+		my $z = $y->(1:$Nexc);
 		$t = davg($y($Nexc:$Nexc+1));
 		$z = $z-$t;
+		
 		$frac = $Nexc/$N;
 		# Fitting the tail and computing the Pvalue
-		($a,$k,$cov) = gpdfit_MOM($z);
+		($a,$k,$cov) = Jin_ZHANG_pareto_fit($z);
+		#print "$a $k $cov\n";
 		my $p = 1 - gsl_cdf_pareto_P($z,$a,$k);
 		($PW2_less_005,$PA2_less_005,$W2,$A2) = gpdgoft( $p ,$k); # goodness-of-fit test
+		#print "$PW2_less_005,$PA2_less_005,$W2,$A2\n";
+		
+		if ( $A2 eq 'nan'){
+			$fitted = $PA2_less_005;
+		} else {
+			$fitted = $PW2_less_005;
+		}
 		$Nexc -= 10;
-	} until ( ( $PA2_less_005 == 1 ) or ($Nexc < 10) );	
+	} until ( ( $fitted == 1 ) or ($Nexc < 11) );	
 	if ($Nexc < 10){
+		print "did not converged\n";
+		getc;
 		return(-1,-1,-1);
 	} else {
 		my ($Phat,$Phatci_low,$Phatci_up) = gpdPval($x0-$t,$a,$k,$cov,$alpha);
@@ -77,110 +88,23 @@ sub gpdPval {
 
 
 	my $Phat = 1 - gsl_cdf_pareto_P($x0,$a,$k);
-
-	# calculate confidence intervals
-	my ($D,$V) = eigens $cov; 
-	$V = real($V); 
-	$D = real($D);
+	
 	my $gaussian_random_mat = grandom 2, 10000; 
 	my $reapmat= ones 2, 10000;
 	my $par = pdl $a,$k;
 	$reapmat = $reapmat * $par; 
+
 	my $chol = mchol($cov);
-	my $Q = real (($gaussian_random_mat  x $chol->transpose ) + $reapmat);
+	my $Q = ($gaussian_random_mat  x $chol->transpose ) + $reapmat;
 	my $sample =  1 - gsl_cdf_pareto_P($x0,$Q->(0,),$Q->(1,));
 	$sample->inplace->setnantobad;
 	$sample =  $sample->where(  $sample->isgood() );
+	
 	my $Phatci_low = pct($sample,$alpha/2);
 	my $Phatci_up =  pct($sample,1-$alpha/2);	
+	
 	return($Phat,$Phatci_low,$Phatci_up);	
 }
-	
-
-
-sub gpdgoft{
-	my $p = shift;
-	my $k = shift;
-	
-	$p = qsort $p;
-	my ($W2,$A2) =  fit_stats($p);
-		
-	my $W2_less_005 = 0; # 0 no and 1 yes
-	my $A2_less_005 = 0; # 0 no and 1 yes
-	
-	$k = 0.5 if ($k < 0.5);
-	
-	my $k_vals = pdl (-0.9,-0.5,-0.2,-0.1,0,0.1,0.2,0.3,0.4,0.5);
-	my $W2_vals  = pdl (0.115,0.124,0.137,0.114,0.153,0.16,0.171,0.184,0.201,0.222);
-	my $A2_vals = pdl (0.771,0.83,0.903,0.935,0.974,1.02,1.074,1.14,1.221,1.321);
-
-	if ($k < -0.9) {
-		$A2_less_005 = 1 if ( $A2 >= 0.771);
-		$W2_less_005 = 1 if ( $W2 >= 0.115);
-	} else {
-		my $critic_w2 = undef;
-		my $critic_a2 = undef;
-		for (my $i = 0; $i < $k_vals->nelem - 1; $i++ ){
-			if ( (  $k >= $k_vals->($i) ) and ( $k_vals->($i + 1) >= $k ) ){
-				$critic_w2 = (  ( $k_vals->($i + 1)-$k_vals->($i) )/($W2_vals->($i + 1) - $W2_vals->($i) )   )*$k + $W2_vals->($i + 1);
-				$critic_a2 = (  ( $k_vals->($i + 1)-$k_vals->($i) )/($A2_vals->($i + 1) - $A2_vals->($i) )   )*$k + $A2_vals->($i + 1);
-			}
-		}
-		$A2_less_005 = 1 if ( $A2 >= $critic_a2);
-		$W2_less_005 = 1 if ( $W2 >= $critic_w2);
-	}
-	return($A2_less_005,$W2_less_005,$W2,$A2);
-}
-
-sub fit_stats { 
-	my $p = shift;
-	my $n = $p->nelem;
-	my $i = 1 + flat sequence 1, $n;
-	# Cramer-von Mises statistic
-	my $W2 = dsum( ($p - ( (2*$i-1)/(2*$n) ))^2 ) + 1/(12*$n);
-	# Anderson Darling statistic
-	my $A2 = -$n -  (1/$n) * dsum( ((2*$i-1) * (log($p)) + log(1- $p->($n+1-$i - 1)) ));
-
-	return($W2,$A2);
-}
-
-
-
-sub gpdfit {
-
-	my ($a,$k) = $z->mme_pareto();
-	my $cov = null;
-	# Compute confidence intervals
-	if (not defined $k){ #k==1 (boundary maximum)
-		$cov = null; #NaN*ones(2);
-    } elsif ($k< 0.5) { # k<.5  All practical cases analyzed so far
-		$cov = pareto_cov($a,$k,$z->nelem);
-    } else {# 0.5<k<1
-		die ("Not implemented\n");
-        $cov = '';#bootstrp(100,@gpdfit,z);
-    }
-	return($a,$k,$cov);
-}
-
-
-sub gpdfit_MOM {
-	my $z = shift;
-	
-	my $xbar = davg($z);
-	my $n = $z->nelem;
-	my $s2 = var($z);
-	#my $a = 0.5*$xbar*(($xbar**(2/$s2)) +1);
-	#my $k = 0.5*(($xbar**(2/$s2)) -1);
-	my ($a,$k) = $z->mme_pareto();
-	my $cov = zeroes 2,2;
-	$cov(0,0) .= 2*($a**2)*(1+6*$k+12*($k**2));
-	$cov(1,0) .= $a*(1+2*$k)*(1+4*$k+12*($k**2));
-	$cov(0,1) .= $cov->(1,0);
-	$cov(1,1) .= ((1+2*$k)**2)*(1+$k+6*($k**2));
-	$cov .= ($cov*(1+$k)**2)/($n*(1+2*$k)*(1+3*$k)*(1+4*$k));
-	return($a,$k,$cov);
-}
-
 
 
 
@@ -193,7 +117,7 @@ sub Jin_ZHANG_pareto_fit {
 	my $xp = $x->(rint($n*(1-$p)-0.5));
 	my $m = 20 + rint( sqrt($n) );
 	my $xq = $x->(rint($n*(1-$p*$p)+0.5));
-	my $k = log($xq/$xp-1)/log($p); 
+	my $k = log($xq/$xp-1)/log($p);
 	$k->inplace->setnantobad;
 	$k->inplace->setbadtoval(0);
 	my $a = $k*$xp/(1-$p**$k);
@@ -217,9 +141,16 @@ sub Jin_ZHANG_pareto_fit {
 	my $w = flat pdl @tmp;
 	$b = dsum($b*$w);
 	$k = -davg(log(1-$b*$x));
-	return($k/$b, $k);
-}
+	my $sigma = $k/$b;
 	
+	my $cov = mones 2,2;
+	$cov(0,0) .= 2*($sigma**2)*(1-$k);
+	$cov(1,0) .= $sigma*(1 - $k);
+	$cov(0,1) .= $sigma*(1 - $k);
+	$cov(1,1) .= (1 - $k)**2;
+	return($sigma, $k,$cov);
+}
+
 sub lx {
 	my $b= shift;
 	my $x = shift;
@@ -231,59 +162,61 @@ sub lx {
 		return( $k-1+log($b/$k) );
 	} 
 }	
+
+
+
+
+sub gpdgoft{
+	my $p = shift;
+	my $k = shift;
 	
+	$p = qsort $p;
+	$p->where( (1 - $p) < 2.2e-16 ) .= 2.2e-16;
 
+	my ($W2,$A2) =  fit_stats($p);
+		
+	my $W2_less_005 = 0; # 0 no and 1 yes
+	my $A2_less_005 = 0; # 0 no and 1 yes
+	
+	$k = 0.5 if ($k > 0.5);
+	
+	my $k_vals = pdl (-0.9,-0.5,-0.2,-0.1,0,0.1,0.2,0.3,0.4,0.5);
+	my $W2_vals  = pdl (0.115,0.124,0.137,0.114,0.153,0.16,0.171,0.184,0.201,0.222);
+	my $A2_vals = pdl (0.771,0.83,0.903,0.935,0.974,1.02,1.074,1.14,1.221,1.321);
 
-
-sub likehood {
-	my($ak) = @_;
-	$ak = pdl $ak;
-	my ($n_vertex,$n_simplex) = $ak->dims; # get dimensions of matrix
-	if (not defined $n_simplex){
-		$n_simplex = 1;
-	}
-	my $back = []; # to send data back
-	# for each simplex get the likehood
-	for (my $s= 0; $s < $n_simplex; $s++){
-		my ($a,$k) = $ak->(,$s)->list;
-		my $n = $z->nelem;
-		my $m = max($z);
-		my $L = '';
-		my $max_test = 0;
-		if (0 < $k*$m){
-			$max_test = $k*$m;
-		}
-		if ($k > 1){
-			$L = -Inf;
-		} elsif ($a < $max_test){# %Constrained space A in Grimshaw
-			$L = -Inf;
-		} else {
-			if (abs($k)< 2.2e-16){ # k==0;
-				$L = -$n*log($a)-(1/($a*dsum($z)));
-			} else {
-				$L = -$n*log($a) + (1/$k-1)*dsum(log(1-$k*$z/$a));
+	if ($k < -0.9) {
+		$A2_less_005 = 1 if ( $A2 >= 0.771);
+		$W2_less_005 = 1 if ( $W2 >= 0.115);
+	} elsif ( $k == 0.5) {
+		$A2_less_005 = 1 if ( $A2 >= 1.321);
+		$W2_less_005 = 1 if ( $W2 >= 0.222);			
+	} else {
+		my $critic_w2 = undef;
+		my $critic_a2 = undef;
+		for (my $i = 0; $i < $k_vals->nelem - 1; $i++ ){
+			if ( (  $k >= $k_vals->($i) ) and ( $k_vals->($i + 1) >= $k ) ){
+				$critic_w2 = (  ( $k_vals->($i + 1)-$k_vals->($i) )/($W2_vals->($i + 1) - $W2_vals->($i) )   )*$k + $W2_vals->($i + 1);
+				$critic_a2 = (  ( $k_vals->($i + 1)-$k_vals->($i) )/($A2_vals->($i + 1) - $A2_vals->($i) )   )*$k + $A2_vals->($i + 1);
 			}
 		}
-		$L = -$L;
-		#print "$a $k $L\n";
-		push @$back, $L;  
+		$A2_less_005 = 1 if ( $A2 >= $critic_a2);
+		$W2_less_005 = 1 if ( $W2 >= $critic_w2);
 	}
-	$back = pdl $back; # send back as piddle
-	return($back->flat);	
+	return($A2_less_005,$W2_less_005,$W2,$A2);
+}
+
+sub fit_stats { 
+	my $p = shift;
+	my $n = $p->nelem;
+	my $i = 1 + flat sequence 1, $n;
+	# Cramer-von Mises statistic
+	my $W2 = dsum( ($p - ( (2*$i-1)/(2*$n) ))^2 ) + 1/(12*$n);
+	# Anderson Darling statistic
+	my $A2 = -$n -  (1/$n) * dsum( ((2*$i-1) * (log($p)) + log(double 1 - double $p->($n+1-$i - 1)) ));
+
+	return($W2,$A2);
 }
 
 
-sub pareto_cov {#= asym_var(parmhat,n);
-	my $a = shift;
-	my $k = shift;
-	my $n = shift;
-	my $cov = zeroes 2,2;
-	$cov->(0,0) .= 2*$a**(2/(1-$k));
-	$cov->(1,0) .= $a*(1-$k);
-	$cov->(0,1) .= $cov->(1,0);
-	$cov->(1,1) .= (1-$k)**2;
-	$cov = $cov/$n;
-	return ($cov);
-}
 
 1;

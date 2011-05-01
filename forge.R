6f9b61d3e886f630a1c7ebabf8c4f11def1143b3
inti <intipedroso@gmail.com>
2011-05-01 10:54:14 +0100

    new version

--- forge.R
+++ forge.R
@@ -4,11 +4,20 @@ flush=10
 max_step<-10000
 step<-10
 MAX<-1000000
-bfile="hm3_ceu.chr22"
+distance<-20
 save_annot<-"FALSE"
 affy2rsid<-"FALSE"
+header<-"FALSE"
 snp_annot<-NULL
 gene_annot<-NULL
+
+bfile="hm3_ceu.chr22"
+assoc_file="~/DATA/WTCCC/WEB/basic/WTCCC_BD_imputed.txt_plusQC.filtered.pvals"
+report=10
+#snp_annot="29April2011.snp_annot.txt"
+#gene_annot="29April2011.gene_annot.txt"
+
+
 # this block parses command line option in to the program.
 # an option like : option=value in transformed in a variable option with value = value
 commandArgs()
@@ -35,7 +44,8 @@ flush<-as.numeric(flush)
 max_step<-as.numeric(max_step)
 step<-as.numeric(step)
 MAX<-as.numeric(MAX)
-
+distance<-as.numeric(distance)*1000
+header<-as.logical(header)
 ######
 cat("Loading necessary libraries\n")
 cat("snpMatrix\n")
@@ -66,28 +76,10 @@ if (is.null(gene_annot) ==FALSE){
     genemart = useMart("ensembl", dataset = "hsapiens_gene_ensembl")
     ENSEMBL_GENES<-getBM(attributes = c("ensembl_gene_id","hgnc_symbol", "chromosome_name", "start_position","end_position", "band","gene_biotype"), mart = genemart)
 }
-# get list of all chromosomes
-all_chrs<-unique(ENSEMBL_GENES$chromosome_name)
-snp<-NULL
-if (is.null(snp_annot) ==FALSE){
-    cat("Reading SNP annotation from [ ", snp_annot," ]\n",sep="")
-    snp<-read.table(snp_annot,header=T,sep="\t")
-} else {
-    cat("Annotating SNPs\n")
-    snpmart = useMart("snp", dataset = "hsapiens_snp")
-    snp<-getBM(c("refsnp_id","chr_name","chrom_start", "chrom_strand","mapweight","allele"), filters="refsnp",values=colnames(genotypes),mart = snpmart)
-}
-
-if (save_annot!="FALSE"){
-    cat("Saving Gene and SNP annotation to [ ",save_annot,".gene_annot.txt ] and [ ",save_annot,".snp_annot.txt ]\n",sep="")
-    snp_out<-paste(save_annot,".snp_annot.txt",sep="")
-    gene_out<-paste(save_annot,".gene_annot.txt",sep="")
-    write.table(snp,file=snp_out,sep="\t",col.names=T,row.names=F,quote=F)
-    write.table(ENSEMBL_GENES,file=gene_out,sep="\t",col.names=T,row.names=F,quote=F)
-}
-assoc_file<-"~/DATA/WTCCC/WEB/basic/WTCCC_CD_imputed.txt_plusQC.filtered.pvals"
 cat("Reading SNP p-values from [ ",assoc_file," ]\n",sep="")
-assoc<-read.table(assoc_file)
+assoc<-read.table(assoc_file,header=header)
+cat("   '-> [ ",nrow(assoc)," ] rows read\n",sep="")
+
 if(ncol(assoc) < 2){
   cat("Only found one columns in file")
   exit()
@@ -100,23 +92,52 @@ if (affy2rsid != "FALSE"){
     cat("Matching Affy ids with file [ ",affy2rsid," ]\n")
     affy2rsid<-read.table(affy2rsid)
     colnames(affy2rsid)<-c("affy","SNP")
+    cat("   '-> Matching ids\n")
     with_affy_ids<-merge(affy2rsid,assoc,by.x="SNP",by.y="SNP")
+    cat("   '-> Adding ids to data\n")
     assoc<-rbind(assoc,with_affy_ids[,c("SNP","P")])
 }
 
-snp<-merge(snp,assoc,by.x="refsnp_id",by.y="SNP")
+# get set of SNPs with stats and genotypes
+genotyped_snps<-as.data.frame(colnames(genotypes))
+colnames(genotyped_snps)<-c("SNP")
+working_snps<-merge(genotyped_snps,assoc,by.x="SNP",by.y="SNP")
+cat("[ ",nrow(working_snps)," ] SNPs with genotypes and statitics\n",sep="")
+
+cat("Deleting genotypes of unused SNPs\n",sep="")
+#genotypes<-genotypes[,working_snps$SNP]
+snp<-NULL
+if (is.null(snp_annot) ==FALSE){
+    cat("Reading SNP annotation from [ ", snp_annot," ]\n",sep="")
+    snp<-read.table(snp_annot,header=T,sep="\t")
+} else {
+    cat("Annotating SNPs\n")
+    snpmart = useMart("snp", dataset = "hsapiens_snp")
+    snp<-getBM(c("refsnp_id","chr_name","chrom_start", "chrom_strand","mapweight","allele"), filters="refsnp",values=working_snps$SNP,mart = snpmart)
+}
+
+if (save_annot!="FALSE"){
+    cat("Saving Gene and SNP annotation to [ ",save_annot,".gene_annot.txt ] and [ ",save_annot,".snp_annot.txt ]\n",sep="")
+    snp_out<-paste(save_annot,".snp_annot.txt",sep="")
+    gene_out<-paste(save_annot,".gene_annot.txt",sep="")
+    write.table(snp,file=snp_out,sep="\t",col.names=T,row.names=F,quote=F)
+    write.table(ENSEMBL_GENES,file=gene_out,sep="\t",col.names=T,row.names=F,quote=F)
+}
+# reduce SNP set to the working set
+snp<-merge(snp,working_snps,by.x="refsnp_id",by.y="SNP")
 
 DATA_OUT<-NULL
 header<-c("ensembl_gene_id","hgnc_symbol","chromosome_name","start_position","end_position","band","gene_biotype","SIDAK","FISHER","Z_FIX","Z_RANDOM","SEEN_SIDAK","SEEN_FISHER","SEEN_Z_FIX","SEEN_Z_RANDOM","N","I2","Q","tau_squared","N_SNPs")
 write.table(t(header), file=outfile,append=F,col.names=F,row.names=F,quote=F,sep="\t")
+# get list of all chromosomes
+all_chrs<-unique(ENSEMBL_GENES$chromosome_name)
+gene_counter=0
 # loop over all chromosomes
-#all_chrs<-c(22)
 for(chr in all_chrs){ 
     # select SNPs in the chromosome
     snp_chr<-snp[which(snp$chr_name == chr),]
     # check if there are SNPs mapped to the chromsome
     if ( nrow(snp_chr) == 0){ 
-        #cat("No SNPs mapped on chromsome [ ",chr," ]\n",sep="")
         next
     }
     cat("Woking on chromsome [ ",chr," ]\n",sep="")
@@ -127,16 +148,15 @@ for(chr in all_chrs){
     gene_chr<-ENSEMBL_GENES[which(ENSEMBL_GENES$chromosome_name==chr),]
     # Map SNPs to genes
     cat("Map SNPs to Genes\n")
-    gene_range<-IRanges(start=gene_chr$start_position,end=gene_chr$end_position)
+    gene_range<-IRanges(start=gene_chr$start_position - distance ,end=gene_chr$end_position + distance)
     map<-findOverlaps(query=gene_range,subject=snp_range,type="any",select="all")
     map<-as.matrix(map)
     cat("[ ",length(unique(map[,1]))," ] genes mapped to SNPs with genotypes and association results\n");
     cat("Starting to Analyse Genes\n")
     # loop over each gene and perform the analyses
-    counter<-0
     for ( my_gene_index in unique(map[,1])){
-        report_advance(counter,report,"Genes")
-        report<-report+1
+        report_advance(gene_counter,report,"Genes")
+        gene_counter<-gene_counter+1
         gene_data<-gene_chr[my_gene_index,]
         # get snps in the gene
         gene_snps<-snp_chr[map[which(map[,1]==my_gene_index),2],]
@@ -144,10 +164,13 @@ for(chr in all_chrs){
             #cat("No SNPs mapped to gene [ ",my_gene," ]\n",sep="")
             next
         } else {
-            cat(nrow(gene_snps)," SNPs mapped to gene [ ",gene_data$ensembl_gene_id," ]\n",sep="")  
+            cat(nrow(gene_snps)," SNPs mapped to gene [ ",gene_data$ensembl_gene_id," ",gene_data$hgnc_symbol," ]\n",sep="")  
         }
         if (nrow(gene_snps) == 1){
             MND_P<-rep(gene_snps$P,4)
+            SEEN<-rep(-1,4)
+            TOTAL<-0
+            ready<-unlist(c(gene_data,MND_P,SEEN,TOTAL,-1,-1,-1,1))
         } else {
             # extract genotypes
             gene_genotypes<-as(genotypes[,gene_snps$refsnp_id],'numeric')
@@ -205,9 +228,9 @@ for(chr in all_chrs){
                 #cat(TOTAL,SEEN,"\n")
                 if (TOTAL > MAX){ break;}
             }# while 
+            MND_P<-(SEEN+1)/(TOTAL+1)
+            ready<-unlist(c(gene_data,MND_P,SEEN,TOTAL,Z_methods$I2,Z_methods$Q,Z_methods$tau_squared,nrow(gene_genotypes_cor)))
         } # else more than 1 SNP 
-        MND_P<-(SEEN+1)/(TOTAL+1)
-        ready<-unlist(c(gene_data,MND_P,SEEN,TOTAL,Z_methods$I2,Z_methods$Q,Z_methods$tau_squared,nrow(gene_genotypes_cor)))
         if (is.null(DATA_OUT) == TRUE) { 
             DATA_OUT<-as.data.frame(t(ready))  
         } else {
